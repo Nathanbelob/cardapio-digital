062ab0301d7696c88fb8274dc3cf89930e3d543d
Nathan <nathanbelo97@gmail.com>
2019-09-14 23:26:56 -0300

    tela principal

--- App.js
+++ App.js
@@ -6,8 +6,9 @@
  * @flow
  */
 
-import React, { Fragment, useState, fetchAll } from 'react';
-import axios from 'axios';
+import React, { Fragment, useState, useEffect } from 'react';
+import { List, ListItem } from 'react-native-elements';
+// import axios from 'axios';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,61 +16,109 @@ import {
   View,
   Text,
   StatusBar,
-  FlatList
+  FlatList,
+
 } from 'react-native';
-import { List, ListItem } from 'react-native-elements'
 
 function App() {
 
-  fetchAll(
-    axios.get('https://api.cardapiodig.com.br/api/v1/produtos')
-    .then(function (response) {
-      response.data.forEach(function (value) {
-        console.log(value)
-        list.push(value);
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    ,[]);
-  const [value, setValue] = useState();
-  const [list] = useState([])
-  const list2 = [
-    {
-      name: 'Amy Farha',
-      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-      subtitle: 'Vice President'
-    },
-  ]
+  // axios.get('https://api.cardapiodig.com.br/api/v1/produtos')
+  //   .then(function (response) {
+  //     response.data.forEach(function (value) {
+  //       console.log(value);
+  //       list.push(value);
+  //     });
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 
-  
-    axios.get('https://api.cardapiodig.com.br/api/v1/produtos')
-      .then(function (response) {
-        response.data.forEach(function (value) {
-          console.log(value)
-          list.push(value);
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    
-    console.log(list)
-    console.log(list2)
+  const [list] = useState([
+    {
+      name: 'Skol',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Brahma',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Antartica Original',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Bohemia',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Heinekein',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Kaiser',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Antartica Original',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Bohemia',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Heinekein',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Kaiser',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Bohemia',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Heinekein',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+    {
+      name: 'Kaiser',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Bebida'
+    },
+  ])
+
   return (
-    <View>
-      {
-        list.map((l) => (
-          <ListItem
-            key={l.id}
-            title={l.nome}
-          />
-        ))
-      }
-    </View>
-  );
-};
+<View>
+<ScrollView>
+  {
+    list.map((l, i) => (
+      <ListItem
+        key={i}
+        leftAvatar={{ source: { uri: l.avatar_url } }}
+        title={l.name}
+        subtitle={l.subtitle}
+        bottomDivider
+      />
+    ))
+  }
+  </ScrollView>
+</View>
+  )
+}
 export default App;
 
 const styles = StyleSheet.create({
